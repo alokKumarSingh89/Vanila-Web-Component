b95e4125669a07a97ffaf4220a105fe339bcf587
alokKumarSingh89 <itsaloksigh2011@gmail.com>
2019-10-20 09:53:51 +0800

    final

---
+++ dist/modal.966b26e9.js
@@ -0,0 +1,425 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"src/modal.js":[function(require,module,exports) {
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Modal =
+/*#__PURE__*/
+function (_HTMLElement) {
+  _inherits(Modal, _HTMLElement);
+
+  function Modal() {
+    var _this;
+
+    _classCallCheck(this, Modal);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Modal).call(this));
+
+    _this.attachShadow({
+      mode: "open"
+    });
+
+    _this.shadowRoot.innerHTML = "\n              <style>\n              #backdroup{\n                position:fixed;\n                top:0;\n                left:0;\n                width:100%;\n                height:100vh;\n                background-color: rgba(0,0,0,.75);\n                z-index:10;\n                opacity:0;\n                pointer-events: none;\n              }\n              #modal{\n                position:fixed;\n                top:15vh;\n                left:25%;\n                width:50%;\n                background:white;\n                border-radius:3px;\n                box-shadow:0 2px 8px rgba(0,0,0,.25);\n                z-index:100;\n                opacity:0;\n                pointer-events: none;\n                transition: all 0.3s ease-out;\n\n              }\n              :host([opened]) #backdroup,\n              :host([opened]) #modal{\n                opacity:1;\n                pointer-events: all;\n              }\n              :host([opened]) #modal{\n                top:25vh;\n              }\n              #actions{\n                border-top:1px solid #ccc;\n                padding:1rem;\n                display:flex;\n                justify-content: flex-end;\n              }\n              #actions button{\n                padding:5px;\n                margin:5px;\n                border-radius:4px;\n                font-size:14px;\n                cursor:pointer;\n              }\n              header,main{\n                padding:.2rem 1rem;\n              }\n            </style>\n            <div id=\"backdroup\"></div>\n            <div id=\"modal\">\n              <header>\n                <h1>Please Confirm</h1>\n              </header>\n              <section id=\"main\">\n                <slot></slot>\n              </section>\n              <section id=\"actions\">\n                <button id=\"cancel-btn\">Cancel</button>\n                <button id=\"confirm-btn\">Yes</button>\n              </section>\n            </div>\n          ";
+
+    var cancelBtn = _this.shadowRoot.querySelector("#cancel-btn");
+
+    var confirmBtn = _this.shadowRoot.querySelector("#confirm-btn");
+
+    cancelBtn.addEventListener('click', _this._cancel.bind(_assertThisInitialized(_this)));
+    confirmBtn.addEventListener('click', _this._confirm.bind(_assertThisInitialized(_this)));
+    return _this;
+  } // attributeChangedCallback(name, oldValue, newValue) {
+  //     if (name === "opened") {
+  //         if (this.hasAttribute("opened")) {
+  //             this.shadowRoot.querySelector("#backdroup").style.opacity = 1;
+  //             this.shadowRoot.querySelector("#backdroup").style.pointerEvents = 'all';
+  //             this.shadowRoot.querySelector("#modal").style.opacity = 1;
+  //             this.shadowRoot.querySelector("#modal").style.pointerEvents = 'all';
+  //         }
+  //     }
+  // }
+  // static get observedAttributes() {
+  //     return ["opened"]
+  // }
+
+
+  _createClass(Modal, [{
+    key: "open",
+    value: function open() {
+      this.setAttribute("opened", "");
+    }
+  }, {
+    key: "hide",
+    value: function hide() {
+      this.removeAttribute("opened");
+    }
+  }, {
+    key: "_cancel",
+    value: function _cancel(event) {
+      this.hide();
+      var CancelEvent = new Event('cancel', {
+        bubbles: true,
+        composed: true
+      });
+      event.target.dispatchEvent(CancelEvent);
+    }
+  }, {
+    key: "_confirm",
+    value: function _confirm() {
+      this.hide();
+      var CancelEvent = new Event('confirm');
+      this.dispatchEvent(CancelEvent);
+    }
+  }]);
+
+  return Modal;
+}(_wrapNativeSuper(HTMLElement));
+
+customElements.define("wc-modal", Modal);
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49413" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/modal.js"], null)
+//# sourceMappingURL=/modal.966b26e9.js.map
--- src/modal.js
+++ src/modal.js
@@ -1,44 +1,106 @@
 class Modal extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-      <style>
-        #backdroup{
-          position:fixed;
-          top:0;
-          left:0;
-          width:100%;
-          height:100vh;
-          background-color: rgba(0,0,0,.75);
-          z-index:10;
+    constructor() {
+            super();
+            this.attachShadow({ mode: "open" });
+            this.shadowRoot.innerHTML = `
+              <style>
+              #backdroup{
+                position:fixed;
+                top:0;
+                left:0;
+                width:100%;
+                height:100vh;
+                background-color: rgba(0,0,0,.75);
+                z-index:10;
+                opacity:0;
+                pointer-events: none;
+              }
+              #modal{
+                position:fixed;
+                top:15vh;
+                left:25%;
+                width:50%;
+                background:white;
+                border-radius:3px;
+                box-shadow:0 2px 8px rgba(0,0,0,.25);
+                z-index:100;
+                opacity:0;
+                pointer-events: none;
+                transition: all 0.3s ease-out;
+
+              }
+              :host([opened]) #backdroup,
+              :host([opened]) #modal{
+                opacity:1;
+                pointer-events: all;
+              }
+              :host([opened]) #modal{
+                top:25vh;
+              }
+              #actions{
+                border-top:1px solid #ccc;
+                padding:1rem;
+                display:flex;
+                justify-content: flex-end;
+              }
+              #actions button{
+                padding:5px;
+                margin:5px;
+                border-radius:4px;
+                font-size:14px;
+                cursor:pointer;
+              }
+              header,main{
+                padding:.2rem 1rem;
+              }
+            </style>
+            <div id="backdroup"></div>
+            <div id="modal">
+              <header>
+                <h1>Please Confirm</h1>
+              </header>
+              <section id="main">
+                <slot></slot>
+              </section>
+              <section id="actions">
+                <button id="cancel-btn">Cancel</button>
+                <button id="confirm-btn">Yes</button>
+              </section>
+            </div>
+          `;
+            const cancelBtn = this.shadowRoot.querySelector("#cancel-btn");
+            const confirmBtn = this.shadowRoot.querySelector("#confirm-btn");
+            cancelBtn.addEventListener('click', this._cancel.bind(this));
+            confirmBtn.addEventListener('click', this._confirm.bind(this));
         }
-        #modal{
-          position:fixed;
-          top:25vh;
-          left:25%;
-          width:50%;
-          height:30rem;
-          background:white;
-          border-radius:3px;
-          box-shadow:0 2px 8px rgba(0,0,0,.25);
-          z-index:100;
-        }
-      </style>
-      <div id="backdroup"></div>
-      <div id="modal">
-        <header>
-          <h1>Please Confirm</h1>
-        </header>
-        <section id="main">
-          <slot></slot>
-        </section>
-        <section id="actions">
-          <button>Cancel</button>
-          <button>Yes</button>
-        </section>
-      </div>
-    `;
-  }
+        // attributeChangedCallback(name, oldValue, newValue) {
+        //     if (name === "opened") {
+        //         if (this.hasAttribute("opened")) {
+        //             this.shadowRoot.querySelector("#backdroup").style.opacity = 1;
+        //             this.shadowRoot.querySelector("#backdroup").style.pointerEvents = 'all';
+        //             this.shadowRoot.querySelector("#modal").style.opacity = 1;
+        //             this.shadowRoot.querySelector("#modal").style.pointerEvents = 'all';
+        //         }
+        //     }
+        // }
+        // static get observedAttributes() {
+        //     return ["opened"]
+        // }
+    open() {
+        this.setAttribute("opened", "");
+    }
+    hide() {
+        this.removeAttribute("opened")
+    }
+    _cancel(event) {
+        this.hide();
+        const CancelEvent = new Event('cancel', { bubbles: true, composed: true });
+        event.target.dispatchEvent(CancelEvent);
+    }
+    _confirm() {
+        this.hide();
+        const CancelEvent = new Event('confirm');
+        this.dispatchEvent(CancelEvent);
+    }
 }
 customElements.define("wc-modal", Modal);
